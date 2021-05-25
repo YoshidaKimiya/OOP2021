3c9c20cb4743c5b5dbaa17c7ac797b2c5efa1399
YoshidaKimiya <32104@ojs.ac.jp>
2021-05-25 11:34:32 +0900

    P58 リスト2.14 売上クラスの作成

---
+++ SalesCalculator/Sales.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SalesCalculator {
+    public class Sales {
+        public string ShopNameA { get; set; }
+
+        public string ProductCategory { get; set; }
+
+        public int Amount { set; get; }
+    }
+}
