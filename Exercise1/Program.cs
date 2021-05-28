24a6d94fda8f4b9fee8e742ecc2365f81e95ab4f
YoshidaKimiya <32104@ojs.ac.jp>
2021-05-28 15:37:34 +0900

    演習問題2.1　完成版

---
+++ Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static Song[] songs = new Song[2];
+        static void Main(string[] args) {
+             
+            songs[0] = new Song("あいみょん", "マリーゴールド", 240);
+            songs[1] = new Song("星野源", "恋", 200);
+            PrintSongs(songs);
+        }
+        private static void PrintSongs(Song[] songs) {
+            foreach (var song in songs) {
+                Console.WriteLine("{0},{1},{2}:{3}",song.Title,song.ArtistName,song.Length/60,song.Length%60);
+            }
+        }   
+    }
+}
---
+++ Exercise1/Song.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Song {
+        private string title;
+        private string artistName;
+        private int length;
+
+        public string Title { get => title; set => title = value; }
+        public string ArtistName { get => artistName; set => artistName = value; }
+        public int Length { get => length; set => length = value; }
+    
+        public Song(string title,string artistName,int length) {
+            this.title = title;
+            this.artistName = artistName;
+            this.length = length;
+        }
+    }
+}
