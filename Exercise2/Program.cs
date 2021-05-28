9dbc6e1cf82ee2b64300ce4a783afaa0854d5db6
YoshidaKimiya <32104@ojs.ac.jp>
2021-05-28 15:59:22 +0900

    演習問題 2.2 完成版

---
+++ Exercise2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            for (int inch = 1; inch <= 10; inch++) {
+                double meter = InchConverter.ToMeter(inch);
+                Console.WriteLine("{0} ih = {1:0.0000} m", inch, meter);
+            }
+
+
+        }
+    }
+}
