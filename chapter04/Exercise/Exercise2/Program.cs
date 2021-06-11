75af4160f4c260e176e905a04c84247cb5db80bd
YoshidaKimiya <32104@ojs.ac.jp>
2021-06-11 15:25:05 +0900

    P134 演習問題 完成版

---
+++ chapter04/Exercise/Exercise2/Program.cs
@@ -0,0 +1,56 @@
+﻿using Exercise1;
+using System;
+using System.Linq;
+
+namespace Exercise2 {
+    class Program {
+        static void Main(string[] args) {
+            var ymCollection = new YearMonth[] {
+                new YearMonth(1980,1),
+                new YearMonth(1990,4),
+                new YearMonth(2001,7),
+                new YearMonth(1970,9),
+                new YearMonth(1990,12),
+            };
+
+            //4.2.2
+            Exercise2_2(ymCollection);
+            Console.WriteLine("-----");
+
+            Exercise2_4(ymCollection);
+            Console.WriteLine("-----");
+
+            Exercise2_5(ymCollection);
+            
+        }
+
+        private static void Exercise2_2(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                Console.WriteLine(ym);
+            }
+
+            
+        }
+        static YearMonth FindFirst21c(YearMonth[] ymCollection) {
+            foreach (var ym in ymCollection) {
+                if (ym.Is21Century) {
+                    return ym;
+                }
+            }
+            return null;
+        }
+        private static void Exercise2_4(YearMonth[] ymCollection) {
+            var yCheck = FindFirst21c(ymCollection);
+            var s = yCheck != null ? yCheck.Year.ToString() : "21世紀のデータはありません。";
+            Console.WriteLine(s);
+        
+        }
+
+        private static void Exercise2_5(YearMonth[] ymCollection) {
+            var select = ymCollection.Select(ym => ym.AddOneMonth()).ToArray();
+            foreach (var ym in select) {
+                Console.WriteLine(ym);
+            }
+        }
+    }
+}
--- chapter04/Section01/Program.cs
+++ chapter04/Section01/Program.cs
@@ -7,6 +7,29 @@ using System.Threading.Tasks;
 namespace Section01 {
     class Program {
         static void Main(string[] args) {
+            var Session = new Dictionary<string, object>();
+
+            var product = Session["MyProduct"] as Product;
+            if(product == null) {
+                Console.WriteLine("productが取得できなかった");
+            }else {
+                Console.WriteLine("productが取得できた");
+            }
+
+
         }
+    
+        private static Product GetProduct() {
+            Sale sale = new Sale();
+            
+            return sale?.Product; //null条件演算子
+        }
+    
+    }
+
+    class Sale {
+        public string ShopName { get; set; } = "abcdef";
+        public int Amount { get; set; } = 12340;
+        public Product Product { get; set; }
     }
 }
