acd8591436a08404572ad2db0bd2ed2f7ba3ee74
YoshidaKimiya <32104@ojs.ac.jp>
2021-06-21 13:25:13 +0900

    １期目試験

---
+++ Chapter05/Exercise1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise1 {
+    class Program {
+        static void Main(string[] args) {
+            #region 問題5.1
+#if false
+            var s1 = Console.ReadLine();
+            var s2 = Console.ReadLine();
+            
+            if (String.Compare(s1, s2, ignoreCase:true) == 0) {
+                Console.WriteLine("等しい");
+            } else {
+                Console.WriteLine("等しくない");
+            }
+#endif
+            #endregion
+            string s1 = Console.ReadLine();
+            string s2 = Console.ReadLine();
+        }
+    }
+}
