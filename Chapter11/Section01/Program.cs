c44e130d9cc8a9ddd5a9359aa85c33d2de52e0ae
公也 吉田 <32104@ojs.ac.jp>
2021-08-26 15:39:32 +0900

    リスト11.7まで

---
+++ Chapter11/Section01/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Section01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var xdoc = XDocument.Load("novelists.xml");
+            
+            //var xelements = xdoc.Root.Elements()
+            //.Where(x => ((DateTime)x.Element("birth")).Year >= 1900);
+            
+            foreach ( var xnovelist in xdoc.Root.Elements())
+            {
+                var xname = xnovelist.Element("name");
+                var works = xnovelist.Element("masterpieces")
+                                      .Elements("title")
+                                      .Select(x => x.Value);
+                
+                Console.WriteLine("{0} - {1}", xname.Value, string.Join (",",works));
+            }
+
+        }
+    }
+}
