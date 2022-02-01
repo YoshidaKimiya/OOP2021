835577964ea709ea34f96d31cf984d6842ad806b
YoshidaKimiya <32104@ojs.ac.jp>
2022-02-01 14:20:12 +0900

    卒業制作途中

--- Sotsug/Form1.Designer.cs
+++ Sotsug/Form1.Designer.cs
@@ -29,121 +29,61 @@ namespace Sotsug
         /// </summary>
         private void InitializeComponent()
         {
-            this.raPop = new System.Windows.Forms.RadioButton();
-            this.raCheap = new System.Windows.Forms.RadioButton();
-            this.raExpensive = new System.Windows.Forms.RadioButton();
-            this.tbSearch = new System.Windows.Forms.TextBox();
-            this.btSearch = new System.Windows.Forms.Button();
-            this.label1 = new System.Windows.Forms.Label();
+            this.components = new System.ComponentModel.Container();
             this.btOuter = new System.Windows.Forms.Button();
             this.btTops = new System.Windows.Forms.Button();
             this.btTrousers = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
             this.button6 = new System.Windows.Forms.Button();
+            this.dgvRegistData = new System.Windows.Forms.DataGridView();
+            this.infosys202116DataSet = new Sotsug.infosys202116DataSet();
+            this.clothingBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.clothingTableAdapter = new Sotsug.infosys202116DataSetTableAdapters.ClothingTableAdapter();
+            this.ブランド名DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.ジャンルDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.カテゴリDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.色DataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.サイズDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.写真DataGridViewImageColumn = new System.Windows.Forms.DataGridViewImageColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistData)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infosys202116DataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clothingBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // raPop
-            // 
-            this.raPop.AutoSize = true;
-            this.raPop.Location = new System.Drawing.Point(12, 28);
-            this.raPop.Name = "raPop";
-            this.raPop.Size = new System.Drawing.Size(59, 16);
-            this.raPop.TabIndex = 0;
-            this.raPop.TabStop = true;
-            this.raPop.Text = "人気順";
-            this.raPop.UseVisualStyleBackColor = true;
-            // 
-            // raCheap
-            // 
-            this.raCheap.AutoSize = true;
-            this.raCheap.Location = new System.Drawing.Point(68, 28);
-            this.raCheap.Name = "raCheap";
-            this.raCheap.Size = new System.Drawing.Size(91, 16);
-            this.raCheap.TabIndex = 1;
-            this.raCheap.TabStop = true;
-            this.raCheap.Text = "価格が安い順";
-            this.raCheap.UseVisualStyleBackColor = true;
-            // 
-            // raExpensive
-            // 
-            this.raExpensive.AutoSize = true;
-            this.raExpensive.Location = new System.Drawing.Point(155, 28);
-            this.raExpensive.Name = "raExpensive";
-            this.raExpensive.Size = new System.Drawing.Size(91, 16);
-            this.raExpensive.TabIndex = 2;
-            this.raExpensive.TabStop = true;
-            this.raExpensive.Text = "価格が高い順";
-            this.raExpensive.UseVisualStyleBackColor = true;
-            // 
-            // tbSearch
-            // 
-            this.tbSearch.Location = new System.Drawing.Point(51, 51);
-            this.tbSearch.Name = "tbSearch";
-            this.tbSearch.Size = new System.Drawing.Size(307, 19);
-            this.tbSearch.TabIndex = 3;
-            // 
-            // btSearch
-            // 
-            this.btSearch.Location = new System.Drawing.Point(379, 47);
-            this.btSearch.Name = "btSearch";
-            this.btSearch.Size = new System.Drawing.Size(45, 26);
-            this.btSearch.TabIndex = 4;
-            this.btSearch.Text = "検索";
-            this.btSearch.UseVisualStyleBackColor = true;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(10, 87);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(32, 12);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "メンズ";
             // 
             // btOuter
             // 
-            this.btOuter.Location = new System.Drawing.Point(28, 102);
+            this.btOuter.Location = new System.Drawing.Point(12, 150);
             this.btOuter.Name = "btOuter";
-            this.btOuter.Size = new System.Drawing.Size(58, 31);
+            this.btOuter.Size = new System.Drawing.Size(163, 31);
             this.btOuter.TabIndex = 7;
             this.btOuter.Text = "アウター";
             this.btOuter.UseVisualStyleBackColor = true;
             // 
             // btTops
             // 
-            this.btTops.Location = new System.Drawing.Point(28, 139);
+            this.btTops.Location = new System.Drawing.Point(12, 114);
             this.btTops.Name = "btTops";
-            this.btTops.Size = new System.Drawing.Size(58, 30);
+            this.btTops.Size = new System.Drawing.Size(163, 30);
             this.btTops.TabIndex = 8;
             this.btTops.Text = "トップス";
             this.btTops.UseVisualStyleBackColor = true;
             // 
             // btTrousers
             // 
-            this.btTrousers.Location = new System.Drawing.Point(28, 175);
+            this.btTrousers.Location = new System.Drawing.Point(12, 78);
             this.btTrousers.Name = "btTrousers";
-            this.btTrousers.Size = new System.Drawing.Size(58, 30);
+            this.btTrousers.Size = new System.Drawing.Size(163, 30);
             this.btTrousers.TabIndex = 9;
             this.btTrousers.Text = "ズボン";
             this.btTrousers.UseVisualStyleBackColor = true;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(219, 103);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 12);
-            this.label3.TabIndex = 13;
-            this.label3.Text = "ブランド検索";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(225, 123);
+            this.button1.Location = new System.Drawing.Point(310, 86);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(67, 22);
             this.button1.TabIndex = 14;
@@ -152,7 +92,7 @@ namespace Sotsug
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(310, 123);
+            this.button2.Location = new System.Drawing.Point(395, 86);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(67, 22);
             this.button2.TabIndex = 14;
@@ -161,7 +101,7 @@ namespace Sotsug
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(225, 151);
+            this.button3.Location = new System.Drawing.Point(310, 122);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(67, 22);
             this.button3.TabIndex = 14;
@@ -170,7 +110,7 @@ namespace Sotsug
             // 
             // button4
             // 
-            this.button4.Location = new System.Drawing.Point(310, 151);
+            this.button4.Location = new System.Drawing.Point(395, 122);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(67, 22);
             this.button4.TabIndex = 14;
@@ -179,7 +119,7 @@ namespace Sotsug
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(225, 179);
+            this.button5.Location = new System.Drawing.Point(310, 159);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(67, 22);
             this.button5.TabIndex = 14;
@@ -188,59 +128,126 @@ namespace Sotsug
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(310, 179);
+            this.button6.Location = new System.Drawing.Point(395, 159);
             this.button6.Name = "button6";
             this.button6.Size = new System.Drawing.Size(67, 22);
             this.button6.TabIndex = 14;
             this.button6.Text = "GU";
             this.button6.UseVisualStyleBackColor = true;
             // 
+            // dgvRegistData
+            // 
+            this.dgvRegistData.AutoGenerateColumns = false;
+            this.dgvRegistData.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvRegistData.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.ブランド名DataGridViewTextBoxColumn,
+            this.ジャンルDataGridViewTextBoxColumn,
+            this.カテゴリDataGridViewTextBoxColumn,
+            this.色DataGridViewTextBoxColumn,
+            this.サイズDataGridViewTextBoxColumn,
+            this.写真DataGridViewImageColumn});
+            this.dgvRegistData.DataSource = this.clothingBindingSource;
+            this.dgvRegistData.Location = new System.Drawing.Point(12, 202);
+            this.dgvRegistData.Name = "dgvRegistData";
+            this.dgvRegistData.RowTemplate.Height = 21;
+            this.dgvRegistData.Size = new System.Drawing.Size(612, 170);
+            this.dgvRegistData.TabIndex = 15;
+            // 
+            // infosys202116DataSet
+            // 
+            this.infosys202116DataSet.DataSetName = "infosys202116DataSet";
+            this.infosys202116DataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // clothingBindingSource
+            // 
+            this.clothingBindingSource.DataMember = "Clothing";
+            this.clothingBindingSource.DataSource = this.infosys202116DataSet;
+            // 
+            // clothingTableAdapter
+            // 
+            this.clothingTableAdapter.ClearBeforeFill = true;
+            // 
+            // ブランド名DataGridViewTextBoxColumn
+            // 
+            this.ブランド名DataGridViewTextBoxColumn.DataPropertyName = "ブランド名";
+            this.ブランド名DataGridViewTextBoxColumn.HeaderText = "ブランド名";
+            this.ブランド名DataGridViewTextBoxColumn.Name = "ブランド名DataGridViewTextBoxColumn";
+            // 
+            // ジャンルDataGridViewTextBoxColumn
+            // 
+            this.ジャンルDataGridViewTextBoxColumn.DataPropertyName = "ジャンル";
+            this.ジャンルDataGridViewTextBoxColumn.HeaderText = "ジャンル";
+            this.ジャンルDataGridViewTextBoxColumn.Name = "ジャンルDataGridViewTextBoxColumn";
+            // 
+            // カテゴリDataGridViewTextBoxColumn
+            // 
+            this.カテゴリDataGridViewTextBoxColumn.DataPropertyName = "カテゴリ";
+            this.カテゴリDataGridViewTextBoxColumn.HeaderText = "カテゴリ";
+            this.カテゴリDataGridViewTextBoxColumn.Name = "カテゴリDataGridViewTextBoxColumn";
+            // 
+            // 色DataGridViewTextBoxColumn
+            // 
+            this.色DataGridViewTextBoxColumn.DataPropertyName = "色";
+            this.色DataGridViewTextBoxColumn.HeaderText = "色";
+            this.色DataGridViewTextBoxColumn.Name = "色DataGridViewTextBoxColumn";
+            // 
+            // サイズDataGridViewTextBoxColumn
+            // 
+            this.サイズDataGridViewTextBoxColumn.DataPropertyName = "サイズ";
+            this.サイズDataGridViewTextBoxColumn.HeaderText = "サイズ";
+            this.サイズDataGridViewTextBoxColumn.Name = "サイズDataGridViewTextBoxColumn";
+            // 
+            // 写真DataGridViewImageColumn
+            // 
+            this.写真DataGridViewImageColumn.DataPropertyName = "写真";
+            this.写真DataGridViewImageColumn.HeaderText = "写真";
+            this.写真DataGridViewImageColumn.Name = "写真DataGridViewImageColumn";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(445, 221);
+            this.ClientSize = new System.Drawing.Size(637, 384);
+            this.Controls.Add(this.dgvRegistData);
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.btTrousers);
             this.Controls.Add(this.btTops);
             this.Controls.Add(this.btOuter);
-            this.Controls.Add(this.label1);
-            this.Controls.Add(this.btSearch);
-            this.Controls.Add(this.tbSearch);
-            this.Controls.Add(this.raExpensive);
-            this.Controls.Add(this.raCheap);
-            this.Controls.Add(this.raPop);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvRegistData)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.infosys202116DataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.clothingBindingSource)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.RadioButton raPop;
-        private System.Windows.Forms.RadioButton raCheap;
-        private System.Windows.Forms.RadioButton raExpensive;
-        private System.Windows.Forms.TextBox tbSearch;
-        private System.Windows.Forms.Button btSearch;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btOuter;
         private System.Windows.Forms.Button btTops;
         private System.Windows.Forms.Button btTrousers;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
         private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.DataGridView dgvRegistData;
+        private infosys202116DataSet infosys202116DataSet;
+        private System.Windows.Forms.BindingSource clothingBindingSource;
+        private infosys202116DataSetTableAdapters.ClothingTableAdapter clothingTableAdapter;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ブランド名DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn ジャンルDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn カテゴリDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn 色DataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn サイズDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewImageColumn 写真DataGridViewImageColumn;
     }
 }
 
--- Sotsug/Form1.cs
+++ Sotsug/Form1.cs
@@ -12,9 +12,18 @@ namespace Sotsug
 {
     public partial class Form1 : Form
     {
+        BindingList<Clothing> listClothing = new BindingList<Clothing>();
         public Form1()
         {
             InitializeComponent();
+            dgvRegistData.DataSource = listClothing;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: このコード行はデータを 'infosys202116DataSet.Clothing' テーブルに読み込みます。必要に応じて移動、または削除をしてください。
+            this.clothingTableAdapter.Fill(this.infosys202116DataSet.Clothing);
+
         }
     }
 }
